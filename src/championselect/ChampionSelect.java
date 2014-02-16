1b3b62f3421950a33dbd0173dd82ae713c8507d9
Bertrand Brompton <bertrandbrompton@gmail.com>
2014-02-16 20:14:38 +0000

    Center box prototype added

--- src/championselect/ChampionSelect.java
+++ src/championselect/ChampionSelect.java
@@ -13,8 +13,13 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -24,7 +29,7 @@ import javafx.stage.Stage;
  * @author bertrandbrompton
  */
 public class ChampionSelect extends Application {
-    
+    private static final Image ICON_48 = new Image(BorderPaneSample.class.getResourceAsStream("icon-48x48.png"));
     @Override
     public void start(Stage primaryStage) {
         Group root = new Group();
@@ -54,6 +59,36 @@ public class ChampionSelect extends Application {
         borderPane.setBottom(botRectangle);
         
         root.getChildren().add(borderPane);
+        
+        /*VBox vbox = new VBox(8); // spacing = 8
+        vbox.getChildren().addAll(new Button("Cut"), new Button("Copy"), new Button("Paste"));
+        
+        VBox vbox2 = new VBox(8); // spacing = 8
+        vbox2.getChildren().addAll(new Button("Cut"), new Button("Copy"), new Button("Paste"));
+        
+        borderPane.setLeft(vbox);
+        borderPane.setRight(vbox2);*/
+        
+        VBox centerBox = new VBox(8);
+        HBox panelBox = new HBox(8);
+        
+        TilePane tilePane = new TilePane();
+        tilePane.setPrefColumns(3); //preferred columns
+ 
+        Button[] buttons = new Button[100];
+        for (int j = 0; j < buttons.length; j++) {
+            buttons[j] = new Button("", new ImageView(ICON_48));
+            tilePane.getChildren().add(buttons[j]);
+        }
+        
+        Button lockin = new Button("Lock In");
+        Button spells = new Button("Spells");
+        Button runesnmasteries = new Button("Runes and Masteries");
+        
+        panelBox.getChildren().addAll(runesnmasteries, spells, lockin);
+        
+        centerBox.getChildren().addAll(tilePane, panelBox);
+        borderPane.setCenter(centerBox);
         primaryStage.setTitle("Champion Selection Screen");
         primaryStage.show();
     }
