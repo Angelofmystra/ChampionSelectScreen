e5b3c8c7633d06fdd949a88df4ac925910c000ec
Bertrand Brompton <bertrandbrompton@gmail.com>
2014-02-17 03:53:39 +0000

    Added send button and moved the text entry field to the top of the chat window

--- src/championselect/ChatTab.java
+++ src/championselect/ChatTab.java
@@ -5,9 +5,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /**
@@ -21,20 +25,34 @@ public class ChatTab {
     final private Tab tab;
     
     public ChatTab(String playerName){
+        
         tab = new Tab();
         tab.setText(playerName);
         listview = new ListView();
         VBox vbox = new VBox();               
         listview.setItems(chatItems);
-        
+        listview.setStyle("-fx-fill: red;");
+        textField.setText("write crap here");
         textField.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
                 chatItems.add(textField.getText());
                 textField.clear(); // once you send a msg, it disappears from the textfield              
             }
         });
-        tab.setContent(vbox);
-        vbox.getChildren().addAll(listview, textField);
+        ScrollPane sp = new ScrollPane();
+        sp.setContent(vbox);
+        
+        tab.setContent(sp);
+        HBox textFieldBox = new HBox();
+        Button sendButton = new Button("Send");
+        sendButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                chatItems.add(textField.getText());
+                textField.clear(); // once you send a msg, it disappears from the textfield              
+            }
+        });
+        textFieldBox.getChildren().addAll(textField, sendButton);
+        vbox.getChildren().addAll(textFieldBox, listview);
     }
     public Tab getTab(){
         return tab;
